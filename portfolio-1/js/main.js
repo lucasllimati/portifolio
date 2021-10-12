12f304c5ecc1e487acee7f8071799e241ccece93
Lucas Lima <69853124+lucasllimati@users.noreply.github.com>
2021-10-12 14:53:23 -0300

    portfolio popup

--- portfolio-1/js/main.js
+++ portfolio-1/js/main.js
@@ -1,31 +1,56 @@
 // about section tabs
 (() => {
-    const aboutSection = document.querySelector(".about-section"),
+  const aboutSection = document.querySelector(".about-section"),
     tabsContainer = document.querySelector(".about-tabs");
 
-    tabsContainer.addEventListener("click", (event) => {
-        // if event.target contains 'tab-item' class and not contains 'active' class
-        if(event.target.classList.contains("tab-item") &&
-          !event.target.classList.contains("active")){
-            const target = event.target.getAttribute("data-target");
-            // deactivate existing active 'tab-item'
-            tabsContainer.querySelector(".active").classList.remove("outer-shadow", "active");
-            // activate new 'tab-item'
-            event.target.classList.add("active", "outer-shadow")
-            // deactivate existing active 'tab-item'
-            aboutSection.querySelector(".tab-content.active").classList.remove("active")
-            // activate new 'tab-item'
-            aboutSection.querySelector(target).classList.add("active");
-
-
-          }
-    })
+  tabsContainer.addEventListener("click", (event) => {
+    // if event.target contains 'tab-item' class and not contains 'active' class
+    if (
+      event.target.classList.contains("tab-item") &&
+      !event.target.classList.contains("active")
+    ) {
+      const target = event.target.getAttribute("data-target");
+      // deactivate existing active 'tab-item'
+      tabsContainer.querySelector(".active").classList.remove("outer-shadow", "active");
+      // activate new 'tab-item'
+      event.target.classList.add("active", "outer-shadow");
+      // deactivate existing active 'tab-item'
+      aboutSection
+        .querySelector(".tab-content.active")
+        .classList.remove("active");
+      // activate new 'tab-item'
+      aboutSection.querySelector(target).classList.add("active");
+    }
+  });
 })();
 
 // portfolio filter and popup
 (() => {
+  
   const filterContainer = document.querySelector(".portfolio-filter"),
   portfolioItemsContainer = document.querySelector(".portfolio-items"),
   portfolioItems = document.querySelectorAll(".portfolio-item"),
-  popup = document.querySelector(".portfolio-popup")
-})
+  popup = document.querySelector(".portfolio-popup"),
+  prevBtn = popup.querySelector(".pp-prev"),
+  nextBtn = popup.querySelector(".pp-next"),
+  closeBtn = popup.querySelector(".pp-close"),
+  projectDetailsContainer = popup.querySelector(".pp-details"),
+  projectDetailsBtn = popup.querySelector(".pp-project-details-btn");
+
+  let itemIndex, slideIndex, screenshots;
+
+  // filter portifolio items
+  filterContainer.addEventListener("click", (event) => {
+    if(event.target.classList.contains("filter-item") &&
+    !event.target.classList.contains("active")){
+      // deactivate existing active 'filter-item'
+      filterContainer.querySelector(".active").classList.remove("outer-shadow", "active");
+      // activate new 'filter item'
+      event.target.classList.add("active", "outer-shadow");
+      const target = event.target.getAttribute("data-target");
+      portfolioItems.forEach((item) => {
+        console.log(item)
+      })
+    }
+  });
+})();
