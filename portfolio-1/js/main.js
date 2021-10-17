3795bb3fa48f9195fbd57873d4b050ceea7bae27
Lucas Lima <69853124+lucasllimati@users.noreply.github.com>
2021-10-17 11:43:02 -0300

    preloader

--- portfolio-1/js/main.js
+++ portfolio-1/js/main.js
@@ -328,3 +328,12 @@ function bodyScrollingToggle() {
     }
   })
 })();
+
+
+window.addEventListener("load", () => {
+  // preloader
+  document.querySelector(".preloader").classList.add("fade-out");
+  setTimeout(() => {
+    document.querySelector(".preloader").style.display = "none";
+  },600)
+})
